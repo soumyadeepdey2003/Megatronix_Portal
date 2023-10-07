d6fe1a6f6efe223d6e7fde624e96dcfcef7cc95e
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2023-10-07 11:36:47 +0530

    Update

--- Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/MRD/Controller/MrdController.java
+++ Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/MRD/Controller/MrdController.java
@@ -4,12 +4,16 @@ import megatronix.soumya.Megatronix_portal.MRD.Model.MrdModel;
 import megatronix.soumya.Megatronix_portal.MRD.Repo.MrdRepo;
 import megatronix.soumya.Megatronix_portal.MRD.Service.MrdService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.Future;
 
 @Controller
 public class MrdController {
@@ -19,30 +23,33 @@ public class MrdController {
     @Autowired
     private MrdRepo mrdRepository;
 
+    @Async
     @GetMapping("/register")
-    public String showRegistrationForm(Model model) {
+    public Future<String> showRegistrationFormAsync(Model model) {
         model.addAttribute("user", new MrdModel());
-        return "registration";
+        return new AsyncResult<>("registration");
     }
 
-    @GetMapping("/registration-success")
-    public String registrationSuccess(@RequestParam(name = "id") Long userId, Model model) {
-        // Fetch the user by ID and display the success page
-        MrdModel user = mrdRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("User not found"));
-        model.addAttribute("user", user);
-        return "registration-success";
-    }
+//    @Async
+//    @GetMapping("/registration-success")
+//    public Future<String> registrationSuccessAsync(@RequestParam(name = "id") Long userId, Model model) {
+//        // Fetch the user by ID and display the success page
+//        MrdModel user = mrdRepository.findById(userId)
+//                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+//        model.addAttribute("user", user);
+//        return new AsyncResult<>("registration-success");
+//    }
 
+    @Async
     @PostMapping("/register")
-    public String processRegistration(@ModelAttribute("user") MrdModel user, Model model) {
+    public Future<String> processRegistrationAsync(@ModelAttribute("user") MrdModel user, Model model) {
         try {
             // Call the registerUser method
             MrdModel registeredUser = service.registerUser(user);
-            return "redirect:/registration-success?id=" + registeredUser.getId();
+            return new AsyncResult<>("redirect:/registration-success?id=" + registeredUser.getId());
         } catch (IllegalArgumentException e) {
             model.addAttribute("error", e.getMessage());
-            return "registration";
+            return new AsyncResult<>("registration");
         }
     }
 }
