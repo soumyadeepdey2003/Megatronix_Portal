a354f389d03f144479d58c74f2ce0a46ee900227
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2023-10-09 13:43:10 +0530

    Update

--- Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/RD/Controller/RdController.java
+++ Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/RD/Controller/RdController.java
@@ -1,4 +1,31 @@
 package megatronix.soumya.Megatronix_portal.RD.Controller;
 
+
+import megatronix.soumya.Megatronix_portal.RD.Model.Rdmain;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.concurrent.CompletableFuture;
+
+@Controller
 public class RdController {
+    @GetMapping("/rd")
+    @Async
+    public CompletableFuture<String> showForm(Model model) {
+        Rdmain rdmain = new Rdmain();
+        model.addAttribute("rdmain", rdmain);
+        return CompletableFuture.completedFuture("rd");
+    }
+
+    // Handle form submission
+    @PostMapping("/rd")
+    @Async
+    public CompletableFuture<String> submitForm(@ModelAttribute("rdmain") Rdmain rdmain) {
+        String selectedDomain = rdmain.getSelectedDomain();
+        return CompletableFuture.completedFuture("redirect:/rd/" + selectedDomain);
+    }
 }
---
+++ Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/RD/Model/Rdmain.java
@@ -0,0 +1,13 @@
+package megatronix.soumya.Megatronix_portal.RD.Model;
+
+public class Rdmain {
+    private String selectedDomain;
+
+    public String getSelectedDomain() {
+        return selectedDomain;
+    }
+
+    public void setSelectedDomain(String selectedDomain) {
+        this.selectedDomain = selectedDomain;
+    }
+}
