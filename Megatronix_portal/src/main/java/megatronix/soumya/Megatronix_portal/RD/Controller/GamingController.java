881b350ba7eba1f591a3de1facd6b01db04b591a
soumyadeep dey <105117395+soumyadeepdey2003@users.noreply.github.com>
2023-10-18 10:11:24 +0530

    final

--- Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/RD/Controller/GamingController.java
+++ Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/RD/Controller/GamingController.java
@@ -1,7 +1,33 @@
 package megatronix.soumya.Megatronix_portal.RD.Controller;
 
+import megatronix.soumya.Megatronix_portal.RD.Model.GamingModel;
+import megatronix.soumya.Megatronix_portal.RD.Model.GeneralModel;
+import megatronix.soumya.Megatronix_portal.RD.Repo.GamingRepo;
+import megatronix.soumya.Megatronix_portal.RD.Service.GamingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 public class GamingController {
+    @Autowired
+    private GamingService service;
+
+    @PostMapping("/rd/gaming")
+    @Async
+    public CompletableFuture<String> register(@ModelAttribute("Rduser") GamingModel member, Model model) {
+        try {
+            GamingModel registeredMember = service.GamingOnSportRd(member);
+            model.addAttribute("uniqueId", registeredMember.getId());
+            return CompletableFuture.completedFuture("rd-success");
+        } catch (Exception e) {
+            // Handle validation errors
+            return CompletableFuture.completedFuture("gaming");
+        }
+    }
 }
--- Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/RD/Service/GamingService.java
+++ Megatronix_portal/src/main/java/megatronix/soumya/Megatronix_portal/RD/Service/GamingService.java
@@ -1,11 +1,19 @@
 package megatronix.soumya.Megatronix_portal.RD.Service;
 
+import megatronix.soumya.Megatronix_portal.RD.Model.GamingModel;
+import megatronix.soumya.Megatronix_portal.RD.Model.GeneralModel;
 import megatronix.soumya.Megatronix_portal.RD.Repo.GamingRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GamingService {
     @Autowired
     private GamingRepo gaming;
+
+    @Async
+    public GamingModel GamingOnSportRd(GamingModel member) {
+        return gaming.save(member);
+    }
 }
